c801308d785491043d93d9a1267278a6b7cb1c32
dev-miguel-resources <ingenieromiguelch@gmail.com>
2021-11-22 21:51:54 -0300

    finished ShopPage

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import MenuItem from "../menu-item/menu-item.component";
 
+import "./directory.styles.scss";
+
 class Directory extends React.Component {
   constructor() {
     super();
